6f8975e664f1b7fc6429a5f464a19281de794b40
Wilker Flores <50198811+wilkerfx@users.noreply.github.com>
2020-08-10 16:35:41 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DjendaProject',
+    version='0.1.9',
+    packages=[''],
+    url='djenda.mpcp.com',
+    license='GPL-3.0 License ',
+    author='Wilker Flores',
+    author_email='wilkerfx@gmail.com',
+    description='Company contacts search system by painkiller solutions'
+)
